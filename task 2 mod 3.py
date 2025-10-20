358895618d20da59a2e6a1f5c8782694fea22524
krasnorutskiy-day-topcoins228 <ivanignatenko23@gmail.com>
2025-10-20 14:35:40 +0300

    Delete task 2 mod 3.py

--- task 2 mod 3.py
+++
@@ -1,7 +0,0 @@
-count=0
-for number in range(100,999+1):
-    number_str=str(number)
-    if (number_str[0]==number_str[1] or number_str[0]==number_str[2] or number_str[1]==number_str[2]):
-        count += 1
-    print(count)
-
