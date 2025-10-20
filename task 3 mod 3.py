633d2bdec8e15e46c22bce56a767cadec095e56e
krasnorutskiy-day-topcoins228 <ivanignatenko23@gmail.com>
2025-10-20 14:35:48 +0300

    Delete task 3 mod 3.py

--- task 3 mod 3.py
+++
@@ -1,6 +0,0 @@
-count=0
-for i in range(100,9999):
-    i_str=str(i)
-    if(i_str[0]==i_str[1] or i_str[0]==i_str[2]):
-        count += 1
-    print("Количество целых чисел от 100 до 999 с разными цифрами:",count)
